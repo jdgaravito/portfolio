b8214127cd00bde1d95715b328b5c706a3eaf1bb
Juan David Garavito <jdgaravito@outlook.com>
2024-02-17 01:03:23 +0000

    start to explore animations

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKitten</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { Canvas } from '@threlte/core';
+	import Scene from '../lib/components/Scene.svelte';
+</script>
+
+<div class="scene_container">
+    <h1>Welcome to my portfolio again</h1>
+    <p>this is the final time I'm doing this</p>
+	<Canvas>
+		<Scene />
+	</Canvas>
+</div>
+
+
+
+<style>
+    .scene_container {
+        width: 100%;
+        height: 100vh;
+    }
+
+</style>
